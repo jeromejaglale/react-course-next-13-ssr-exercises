39cb910c799e1129552671128c7af9e3cfaf07f4
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-11-17 17:23:14 -0800

    Ex 1: fix error

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -3,7 +3,7 @@ import React from 'react';
 import format from 'date-fns/format';
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+  const [time, setTime] = React.useState(null);
 
   React.useEffect(() => {
     const intervalId = window.setInterval(() => {
@@ -16,7 +16,9 @@ function Clock() {
   }, []);
 
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
+    <>
+      {time ? <p className="clock">{format(time, 'hh:mm:ss.S a')}</p> : ''}
+    </>
   );
 }
 
